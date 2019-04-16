284da1dcb23cc84066063068cc29a6935b59714e
ScottHull <scottdhull21@gmail.com>
2019-04-16 15:05:11 -0400

    Update 4/16/2019.

--- Thesis_Code_3/z_eq_fnc_of_radius.py
+++ Thesis_Code_3/z_eq_fnc_of_radius.py
@@ -1,4 +1,4 @@
-from math import pi, log, sqrt
+from math import pi, log, sqrt, exp
 import pandas as pd
 import numpy as np
 import matplotlib as mpl; mpl.use("Qt5Agg")
@@ -27,6 +27,14 @@ def turbulentVelocity(gravity, droplet_radius, density_droplet, density_melt, Cd
     v = sqrt(((4 * gravity * diameter) / (3 * Cd)) * ((density_droplet - density_melt) / density_melt))
     return v
 
+def frac_eq(radius, diffusivity, settling_velocity, dynamic_viscosity, z, density_melt):
+    kinematic_viscosity = dynamic_viscosity / density_melt
+    num = -0.996 * (diffusivity**(2/3)) * z
+    den = ((2 * radius)**(3/2)) * (settling_velocity**(1/2)) * (kinematic_viscosity**(1/6))
+    f = 1 - exp(num / den)
+    return f
+
+
 etas = [10**-3.5, 10**-1.0]
 # radii = list(np.arange(0.001, 0.02 + 0.001, 0.001))
 radii = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
@@ -35,17 +43,25 @@ gravity_earth = 9.8
 density_droplet = 7800
 density_melt = 3750
 diffusion_coeff = 10**-8
+earth_magma_ocean_depth = 500 * 1000
+radius_vesta = 263 * 1000
+radius_vesta_core = 113 * 1000
+radius_vesta_mantle = radius_vesta - radius_vesta_core
 
 velocities_vesta = []
 z_eqs_vesta = []
 velocities_earth = []
 z_eqs_earth = []
+frac_eq_vesta_list = []
+frac_eq_earth_list = []
 
 for eta in etas:
     velocities_vesta_temp = []
     z_eqs_vesta_temp = []
     velocities_earth_temp = []
     z_eqs_earth_temp = []
+    frac_eq_vesta_list_temp = []
+    frac_eq_earth_list_temp = []
     for i in radii:
         v_earth = turbulentVelocity(gravity=gravity_earth, density_melt=density_melt, density_droplet=density_droplet,
                                     droplet_radius=i)
@@ -55,14 +71,22 @@ for eta in etas:
                                     droplet_radius=i)
         z_vesta = z_eq(radius_droplet=i, dynamic_viscosity=eta, settling_velocity=v_vesta, diffusion_coeff=diffusion_coeff,
                        density_melt=density_melt)
+        frac_eq_vesta = frac_eq(radius=i, dynamic_viscosity=eta, settling_velocity=v_vesta, diffusivity=diffusion_coeff,
+                                z=radius_vesta_mantle, density_melt=density_melt)
+        frac_eq_earth = frac_eq(radius=i, dynamic_viscosity=eta, settling_velocity=v_earth, diffusivity=diffusion_coeff,
+                                z=earth_magma_ocean_depth, density_melt=density_melt)
         velocities_vesta_temp.append(v_vesta)
         z_eqs_vesta_temp.append(z_vesta)
         velocities_earth_temp.append(v_earth)
         z_eqs_earth_temp.append(z_earth)
+        frac_eq_vesta_list_temp.append(frac_eq_vesta)
+        frac_eq_earth_list_temp.append(frac_eq_earth)
     velocities_vesta.append(velocities_vesta_temp)
     z_eqs_vesta.append(z_eqs_vesta_temp)
     velocities_earth.append(velocities_earth_temp)
     z_eqs_earth.append(z_eqs_earth_temp)
+    frac_eq_vesta_list.append(frac_eq_vesta_list_temp)
+    frac_eq_earth_list.append(frac_eq_earth_list_temp)
 
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(111)
@@ -79,6 +103,20 @@ ax1.grid()
 ax1.legend(loc='upper left')
 ax1.set_yscale('log')
 ax1.set_xscale('log')
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+ax2.plot([i for i in radii], frac_eq_vesta_list[0], linewidth=2.0, color='blue', label="Vesta (g=0.25 m/s$^2$, $\eta$=10$^{-3.5}$)")
+ax2.plot([i for i in radii], frac_eq_earth_list[0], linewidth=2.0, color='red', label="Earth (g=9.80 m/s$^2$, $\eta$=10$^{-3.5}$)")
+ax2.plot([i for i in radii], frac_eq_vesta_list[1], linewidth=2.0, color='blue', linestyle="--", label="Vesta (g=0.25 m/s$^2$, $\eta$=10$^{-1.0}$)")
+ax2.plot([i for i in radii], frac_eq_earth_list[1], linewidth=2.0, color='red', linestyle="--", label="Earth (g=9.80 m/s$^2$, $\eta$=10$^{-1.0}$)")
+ax2.set_xlabel("Droplet Radius (m)")
+ax2.set_ylabel("Fraction Equilibrated (%)")
+ax2.set_title("Fraction of Metal-Silicate Chemical Equilibrium as a Function of Droplet Radius")
+ax2.grid()
+ax2.legend(loc='lower left')
+ax2.set_xscale('log')
+ax2.set_yscale('log')
 
 plt.show()
 
--- Thesis_Code_4/diffusion_time.py
+++ Thesis_Code_4/diffusion_time.py
@@ -18,23 +18,27 @@ vesta_radius = 263 * 1000  # m
 vesta_core_radius = 113 * 1000  # m
 vesta_mantle_depth = vesta_radius - vesta_core_radius
 w_diffusivity = 10**(-8)
-lengths = list(np.arange(0, vesta_radius, 100))
-times = [diffusionTime(diffusivity=w_diffusivity, length=int(i)) for i in lengths]
-times_millions_of_years = [(i * (3.17098 * (10**-8))) / (10**6) for i in times]
+lengths = list(np.arange(0, 5000, 10))
+times_1 = [diffusionTime(diffusivity=w_diffusivity, length=int(i)) for i in lengths]
+# times_2 = [diffusionTime(diffusivity=w_diffusivity * (10**-8), length=int(i)) for i in lengths]
+times_1_millions_of_years = [(i * (3.17098 * (10**-8))) / (10**6) for i in times_1]
+# times_2_millions_of_years = [(i * (3.17098 * (10**-8))) / (10**6) for i in times_2]
 
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot([i / 1000 for i in lengths], times_millions_of_years, linewidth=2.0, color='black')
+ax.plot([i / 1000 for i in lengths], times_1_millions_of_years, linewidth=2.0, color='black', label="$D=10^{-8}$")
+# ax.plot([i / 1000 for i in lengths], times_2_millions_of_years, linewidth=2.0, color='green', label="$D=10^{-16}$")
 ax.grid()
 ax.set_title("W Diffusion Time as a Function of Length")
-ax.set_xlabel("Time (Ma)")
-ax.set_ylabel("Length (km)")
-ax.axvline(5, linewidth=2.0, linestyle="--", color="red", label="End of Vesta Metal-Silicate Separation")
-ax.axvline(100, linewidth=2.0, linestyle="-.", color="red", label="Complete Decay of $^{182}$Hf")
+ax.set_xlabel("Length (km)")
+ax.set_ylabel("Time (Ma)")
+# ax.axhline(5, linewidth=2.0, linestyle="--", color="red", label="End of Vesta Metal-Silicate Separation")
+# ax.axhline(100, linewidth=2.0, linestyle="-.", color="red", label="Complete Decay of $^{182}$Hf")
+# ax.axhline(4500, linewidth=2.0, linestyle="--", color='green', label="Present Day")
 # ax.axhline(vesta_mantle_depth / 1000, linewidth=2.0, linestyle="--", color='blue', label="Vesta Mantle Depth")
-ax.axhline(vesta_radius / 1000, linewidth=2.0, linestyle="-.", color='blue', label="Vesta Radius")
-ax.legend(loc='lower right')
+# ax.axvline(vesta_radius / 1000, linewidth=2.0, linestyle="-.", color='blue', label="Vesta Radius")
+ax.legend(loc='upper left')
 
 
 plt.show()
